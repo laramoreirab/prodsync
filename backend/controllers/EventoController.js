129f3a63308880e5972b0ceb2e5b6c2a7f25e774
laramoreirab <laramoreiraba@gmail.com>
2026-04-15 17:23:20 -0300

    fazendo eventos

--- backend/controllers/EventoController.js
+++ backend/controllers/EventoController.js
@@ -15,16 +15,37 @@ class EventoController{
                 ...resultado
             });
         } catch (error) {
-            console.error('Erro ao listar usuários:', error);
+            console.error('Erro ao listar eventos:', error);
             res.status(500).json({
                 sucesso: false,
                 erro: 'Erro interno do servidor',
-                mensagem: 'Não foi possível listar os produtos'
+                mensagem: 'Não foi possível listar os eventos!'
             });
         }
         
     }
-    static async criarEvento(req,res){
-        
+    static async registrarEvento(req,res){
+        try {
+        const id_empresa = req.user.id_empresa;
+        const { status_maquina, id_maquina, timestamp } = req.body;
+
+        const parada =  await EventoModel.registrarEvento(id_empresa,  status_maquina, id_maquina, timestamp)
+
+        res.status(201).json({
+            sucesso: true,
+            mensagem: 'Evento registrao com sucesso!'
+        })
+        } catch (error) {
+            console.error('Erro ao registrar evento:', error);
+            res.status(500).json({
+                sucesso: false,
+                erro: 'Erro interno do servidor',
+                mensagem: 'Não foi possível registrar o evento!'
+            });
+        }
+    }
+
+    static async atualizar(req, res){
+
     }
 }
--- backend/models/EventoModel.js
+++ backend/models/EventoModel.js
@@ -1,8 +1,8 @@
 import prisma from '../config/prisma.js';
 import { paginarPrisma } from '../utils/paginacaoUtil.js';
 
-class EventoModel{
-    static async listarTodos(id_empresa, paginacao){
+class EventoModel {
+    static async listarTodos(id_empresa, paginacao) {
         try {
             const regrasDaBusca = {
                 where: {
@@ -25,6 +25,47 @@ class EventoModel{
             throw error;
         }
     }
+
+    static async converterTimestamp(timestamp) {
+        const ms = String(timestamp).length === 10
+            ? timestamp * 1000   // veio em segundos → converte
+            : timestamp          // veio em milissegundos → usa direto
+
+    }
+
+    static async formatarParada(parada) {
+        return {
+            ...parada,
+            inicio: parada.inicio ? {
+              exibicao: new Date(parada.inicio).toLocaleString('pt-BR'),
+              // ex: "26/03/2024, 14:10"
+              iso: new Date(parada.inicio).toISOString()
+              // ex: "2024-03-26T14:10:00.000Z" — pro timer do frontend
+            } : null,
+            fim: parada.fim ? {
+              exibicao: new Date(parada.fim).toLocaleString('pt-BR'),
+              iso: new Date(parada.fim).toISOString()
+            } : null,
+          }
+
+    }
+
+    static async registrarEvento(id_empresa, status_maquina, id_maquina, datastamp) {
+        const inicio = this.converterTimestamp(datastamp);
+
+        //achar
+
+        const resultado = await prisma.historico_eventos.create({
+            data:{
+                id_empresa,
+                id_maquina,
+
+            }
+        })
+
+        return this.formatarParada(resultado)
+    }
+
 }
 
 export default EventoModel;
