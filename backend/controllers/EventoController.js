067426e996fb048da69a0b6517bc59ce5345a63b
Lara Moreira Barreto <larabarretomoreira.005@gmail.com>
2026-04-16 22:57:28 -0300

    refactor(backend) database(backend): terminando eventoModel e eventoController, adicionando colunas na tabela historico_eventos

--- backend/controllers/EventoController.js
+++ backend/controllers/EventoController.js
@@ -24,12 +24,34 @@ class EventoController{
         }
         
     }
-    static async registrarEvento(req,res){
+    static async registrarEventoMaquina(req,res){
         try {
         const id_empresa = req.user.id_empresa;
         const { status_maquina, id_maquina, timestamp } = req.body;
 
-        const parada =  await EventoModel.registrarEvento(id_empresa,  status_maquina, id_maquina, timestamp)
+        if (!status_maquina || status_maquina.trim() == '') {
+                res.status(400).json({
+                    sucesso: false,
+                    erro: 'Status da máquina é obrigatório',
+                    mensagem: 'O evento não pode ser registrado! Status da máquina é obrigatório!'
+                })
+            }
+        if (!id_maquina || id_maquina.trim() == '') {
+                res.status(400).json({
+                    sucesso: false,
+                    erro: 'ID da máquina é obrigatório',
+                    mensagem: 'O evento não pode ser registrado! ID da máquina é obrigatório!'
+                })
+            }
+        if (!timestamp || timestamp.trim() == '') {
+                res.status(400).json({
+                    sucesso: false,
+                    erro: 'Data/hora do evento é obrigatório',
+                    mensagem: 'O evento não pode ser registrado! Data/hora da máquina é obrigatório!'
+                })
+            }
+
+        const parada =  await EventoModel.registrarEventoMaquina(id_empresa,  status_maquina, id_maquina, timestamp)
 
         res.status(201).json({
             sucesso: true,
@@ -44,6 +66,58 @@ class EventoController{
             });
         }
     }
+
+     static async registrarEventoSistema(req, res){
+         const id_empresa = req.user.id_empresa;
+         const { status_maquina, setor_afetado, id_maquina, inicio, fim, id_motivo_parada, observacao } = req.body
+
+        if (!status_maquina || status_maquina.trim() == '') {
+                res.status(400).json({
+                    sucesso: false,
+                    erro: 'Status da máquina é obrigatório',
+                    mensagem: 'O evento não pode ser registrado! Status da máquina é obrigatório!'
+                })
+            }
+        if (!id_maquina || id_maquina.trim() == '') {
+                res.status(400).json({
+                    sucesso: false,
+                    erro: 'ID da máquina é obrigatório',
+                    mensagem: 'O evento não pode ser registrado! ID da máquina é obrigatório!'
+                })
+            }
+        if (!inicio || inicio.trim() == '') {
+                res.status(400).json({
+                    sucesso: false,
+                    erro: 'Início do evento é obrigatório',
+                    mensagem: 'O evento não pode ser registrado! Data e hora do início do evento é obrigatório!'
+                })
+            }
+        if (!fim || fim.trim() == '') {
+                res.status(400).json({
+                    sucesso: false,
+                    erro: 'Fim do evento é obrigatório',
+                    mensagem: 'O evento não pode ser registrado! Data e hora do fim do evento é obrigatório!'
+                })
+            }
+        if (!id_motivo_parada || id_motivo_parada.trim() == '') {
+                res.status(400).json({
+                    sucesso: false,
+                    erro: 'Motivo de Parada é obrigatório',
+                    mensagem: 'O evento não pode ser registrado! Motivo de Parada é obrigatório!'
+                })
+            }
+        if (!setor_afetado || setor_afetado.trim() == '') {
+                res.status(400).json({
+                    sucesso: false,
+                    erro: 'Setor Afetado é obrigatório',
+                    mensagem: 'O evento não pode ser registrado! Setor Afetado pelo evento é obrigatório!'
+                })
+            }
+
+            const registrarEvento = await EventoModel.registrarEventoSistema(id_empresa, status_maquina, setor_afetado, id_maquina, inicio, fim, id_motivo_parada, observacao )
+
+        }
+
 
     static async justificarEvento(req, res){
 
@@ -73,8 +147,7 @@ class EventoController{
             })
            
         }
-
-        
-    
     }
+
+    //obter evento por id 
 }
