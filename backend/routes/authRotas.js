7cb47fbf5c93ba866783ae013cc43ad194482a25
EnzoPenido <enzopoalencar01@gmail.com>
2026-04-17 17:20:31 -0300

    fix: importações corrigidas

--- backend/routes/authRotas.js
+++ backend/routes/authRotas.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { authMiddleware } from '../middlewares/authMiddleware.js'
-import authController from '../controllers/AuthController.js'
+import authController from '../controllers/authController.js'
 
 const router = express.Router()
 
 router.post('/login', authController.login)
 router.post('/cadastrar', authController.cadastrar)
+
+export default router
