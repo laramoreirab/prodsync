74d7c84f560741a48957e4713b745366564011e2
Giovana Frade <13gialves@gmail.com>
2026-04-22 17:19:26 -0300

    style: estilizando modais de edição e cadastro

--- frontend/src/app/(app)/adm/setores/page.jsx
+++ frontend/src/app/(app)/adm/setores/page.jsx
@@ -162,15 +162,16 @@ export default function PageLayout() {
                 <div className="flex items-center">
                   <div className="bg-blue-900 flex items-center px-4 py-2 rounded-md">
                     <Plus className="mr-2 text-3xl text-white" />
-x                    <DialogTitle className="text-3xl text-white">Criar Setor</DialogTitle>
+                    x                    <DialogTitle className="text-3xl text-white">Criar Setor</DialogTitle>
                   </div>
                 </div>
                 <Separator className="m-2 bg-[#a6a6a6]" />
+                <form className="px-8 pb-8 pt-4 flex flex-col gap-6">
+
+                </form>
               </DialogContent>
 
-              <form className="px-8 pb-8 pt-4 flex flex-col gap-6">
 
-              </form>
 
             </Dialog>
           </div>
