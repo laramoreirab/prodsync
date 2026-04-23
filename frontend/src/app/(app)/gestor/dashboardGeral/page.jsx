a6a7abc854d3dff59858cf54bb90bc75a8eae588
giovanafrade <13gialves@gmail.com>
2026-04-23 17:17:21 -0300

    feat: adicionando pastas de gestor e operador

---
+++ frontend/src/app/(app)/gestor/dashboardGeral/page.jsx
@@ -0,0 +1,6 @@
+export default function DashboardGeralGestor() {
+    <>
+        <main className="min-h-screen bg-[url('/bg_app.svg')] bg-cover bg-fixed bg-center bg-no-repeat flex flex-col">
+            <Header /></main>
+    </>
+}
---
+++ frontend/src/app/(app)/operador/dashboardGeral/page.jsx
@@ -0,0 +1,6 @@
+export default function DashboardGeralOperador() {
+    <>
+        <main className="min-h-screen bg-[url('/bg_app.svg')] bg-cover bg-fixed bg-center bg-no-repeat flex flex-col">
+            <Header /></main>
+    </>
+}
