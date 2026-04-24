960d81cd103fd8c52ce3f1cf478e68a8a37b0422
LARISSA OTOMO KLAROSK <25170899@educ123.sp.senai.br>
2026-04-24 14:24:07 -0300

    refactor(front): arrumando as pastas de usuario especifico

--- frontend/src/features/operador/hooks/useOEEOperador.js
+++ frontend/src/features/operador/hooks/useOEEOperador.js
@@ -1,7 +1,6 @@
 "use client";
 import { useChartData } from "@/hooks/useChartData";
 import { oeeOperadorService } from "@services/operadorService";
-import { useCallback } from "react";
  
 export function useOEEOperador(operadorId) {
   return useChartData(oeeOperadorService.getOEE, operadorId);
