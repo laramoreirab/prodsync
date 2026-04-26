36ddd56e18778d4f6db68d658354d7a2eabeee3b
Siss-a <sissaklarosk@gmail.com>
2026-04-25 22:24:06 -0300

    feat(front): adicionando gráficos da página especifica de setores(com alguns problemas de style ainda)

--- frontend/src/app/(app)/adm/setores/[id]/page.jsx
+++ frontend/src/app/(app)/adm/setores/[id]/page.jsx
@@ -25,14 +25,14 @@ export default function SetorEspecificoPage({ params }) {
 
       <div className="w-full max-w-6xl mt-8 pb-10 px-4 space-y-4">
 
-        {/* TÍTULO */}
+
         <div className="flex justify-start mb-6">
           <h1 className="text-4xl font-semibold text-black border-b-4 border-[var(--secondary-foreground)] pb-0 inline-block">
             Setor
           </h1>
         </div>
 
-        {/* SEÇÃO 1 — 2/3 Esquerda, 1/3 Direita */}
+
         <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <div className="md:col-span-2 bg-white border rounded-xl p-6 shadow-sm">
             <SetorMaquinaStatusWidget setorId={id} />
@@ -42,7 +42,7 @@ export default function SetorEspecificoPage({ params }) {
           </div>
         </section>
 
-        {/* SEÇÃO 2 — 2/3 Esquerda, 1/3 Direita */}
+
         <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <div className="md:col-span-2 bg-white border rounded-xl p-6 shadow-sm">
             <SetorOEEEvolucaoWidget setorId={id} />
@@ -52,7 +52,7 @@ export default function SetorEspecificoPage({ params }) {
           </div>
         </section>
 
-        {/* SEÇÃO 3 — INVERTIDO: 1/3 Esquerda, 2/3 Direita */}
+
         <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <div className="md:col-span-1 bg-white border rounded-xl p-6 shadow-sm">
             <SetorMotivosParadaWidget setorId={id} />
