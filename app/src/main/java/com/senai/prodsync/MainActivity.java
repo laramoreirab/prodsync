a4ac85fa90027bf4e9bada9fa65fd752993cd379
biaabgs <goncalvesdasilvabia@gmail.com>
2026-05-14 16:01:10 -0300

    feat(mobile): Adicionei a tab perfil

--- app/src/main/java/com/senai/prodsync/MainActivity.java
+++ app/src/main/java/com/senai/prodsync/MainActivity.java
@@ -13,7 +13,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCopatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
