8a5dd462296dfb8d05a474f50be5c5b76ff4b40f
biaabgs <goncalvesdasilvabia@gmail.com>
2026-05-14 16:02:29 -0300

    refactor(mobile): arrumando MainActivity

--- app/src/main/java/com/senai/prodsync/MainActivity.java
+++ app/src/main/java/com/senai/prodsync/MainActivity.java
@@ -13,7 +13,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCopatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
