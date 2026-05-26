75734212f3956bef2f97910896b1fb89b03434b1
EnzoPenido <enzopoalencar01@gmail.com>
2026-05-26 15:26:44 -0300

    fix(frontend): Pequena correção de texto na criação de setores

--- frontend/src/app/(app)/adm/setores/page.jsx
+++ frontend/src/app/(app)/adm/setores/page.jsx
@@ -191,7 +191,7 @@ export default function PageSetores() {
                     setCriarAberto("turno");
                   }}
                 >
-                  Turno (todos os setores)
+                  Turno Geral
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
