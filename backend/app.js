ac7e518f7f05a58efa47be04822a46916b464529
EnzoPenido <enzopoalencar01@gmail.com>
2026-05-28 13:57:57 -0300

    Cors app.js

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(helmet(
 ));
 
 app.use(cors({
-    origin: 'http://localhost:3000', // Permitir todas as origens. Ajuste conforme necessário. Ex.: 'http://meufrontend.com'
+    origin: 'https://prodsync-six.vercel.app', // Permitir todas as origens. Ajuste conforme necessário. Ex.: 'http://meufrontend.com'
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
     preflightContinue: false, // Não passar para o próximo middleware
